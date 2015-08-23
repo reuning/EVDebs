a0f0422a642ac48ca036f489dbba73d5e8b60448
reuning <kevin.reuning@gmail.com>
2015-08-22 20:34:39 -0400

    Fixed single variable issues

--- Visualization/ViolinPlots.R
+++ Visualization/ViolinPlots.R
@@ -10,11 +10,11 @@ vplot <- function(y=NULL,x=NULL, data=NULL){
     dens <- density(y, na.rm=T)
     
     plot.new()
-    plot.window(xlim=c(-max(dens$y)*1.2, max(dens$y)*1.2 ),ylim=c(min(dens$x), max(dens$y)))
+    plot.window(xlim=c(-max(dens$y)*1.2, max(dens$y)*1.2 ),ylim=c(min(dens$x), max(dens$x)))
     lines(x=dens$y, y=dens$x)
-    lines(x=dens$y, y=dens$x)
+    lines(x=-dens$y, y=dens$x)
     axis(1, at=c(-max(dens$y)*1.2, max(dens$y)*1.2 ), labels=c("",""), lwd.ticks=0, line=1)
-    axis(2)
+    axis(2, lwd.ticks=0, line=1)
     box(lwd=.1, col="gray")
   } else {
     fact <- as.factor(x)
