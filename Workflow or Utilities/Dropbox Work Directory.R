6c9d0daf60feb1a90c2133c8a649569236c6d04a
reuning <kevin.reuning@gmail.com>
2015-08-23 22:29:25 -0400

    Added secondary dropbox location

--- Workflow or Utilities/Dropbox Work Directory.R
+++ Workflow or Utilities/Dropbox Work Directory.R
@@ -1,16 +1,29 @@
 drop.wd <- function(s.loc = "") {
   ### Code adapted from: http://applyr.blogspot.com/2012/08/get-path-to-your-dropbox-folder.html
   ### defaults to setting the dropbox folder itself as the WD.
+  ### currently checks for the dropbox location in to settings folders. 
+  ### The try errors are overly complicated but something with tryCatch wouldn't work. 
+
   if (!require(RCurl)) stop ("You need to install RCurl package.")
-  if (Sys.info()["sysname"]!="Windows") stop("Currently, 'drop.wd' works for Windows only. Sorry.")
+  if (Sys.info()["sysname"]!="Windows") stop("Currently, 'get.dropbox.folder' works for Windows only. Sorry.")
   
-  s.loc <- gsub("/", "\\\\", s.loc) ### convert to \\ which is  what base64 outputs
-  db.file <- paste(Sys.getenv('APPDATA'), '\\Dropbox\\host.db', sep='')
-  base64coded <- readLines(db.file, warn=FALSE)[2]
+  base64coded <- try(
+    {
+      db.file <- paste(Sys.getenv('APPDATA'), '\\Dropbox\\host.db', sep='')
+      suppressWarnings(readLines(db.file))[2]
+    }, silent=T)
+  if(any(attr(base64coded, "class")=="try-error" , !is.null(attr(base64coded, "class")))){
+    base64coded <- try(
+      {
+        db.file <- paste(Sys.getenv('LOCALAPPDATA'), '\\Dropbox\\host.db', sep='')
+        suppressWarnings(readLines(db.file))[2]
+      }, silent=T)
+  }
+  if(any(attr(base64coded, "class")=="try-error", !is.null(attr(base64coded, "class")))) stop("Cannot find Dropbox")
+  
+  
   
   wd <- paste(base64(base64coded, encode=F), s.loc, sep="\\")
   setwd(wd)
-  paste("Work Directory set to:",normalizePath(wd, winslash="/"), sep=" ")
+  paste("Work Directory set to:",wd)
 }
-
-
